49172d4feeb48f55c6f279b57dee13a72f0d31bf
Shakhmurad <muradb356@gmail.com>
2024-06-07 19:50:09 +0400

    add animation in CartTop

--- app/CartTop/cart.js
+++ app/CartTop/cart.js
@@ -10,15 +10,19 @@ function Cart() {
         <div className="cart-block" >
             <div className="cart-img">
                 <img src="./imgs/Group 4.jpg" />
+                <div className="cantent-after">Vacation Home Rental Success</div>
             </div>
             <div className="cart-img">
                 <img src="./imgs/Rectangle Copy 8.png" />
+                <div className="cantent-after">Vacation Home Rental Success</div>
             </div>
             <div className="cart-img">
                 <img src="./imgs/Rectangle Copy 9.png" />
+                <div className="cantent-after">Vacation Home Rental Success</div>
             </div>
             <div className="cart-img">
                 <img src="./imgs/tanya-tulupenko-r7dDkUN_yvU-unsplash 1.png" />
+                <div className="cantent-after">Vacation Home Rental Success</div>
             </div>
         </div>
 
