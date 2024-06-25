032a9cb1cc4c5407dbd2c8db3a6ee05cb355302d
Shakhmurad <muradb356@gmail.com>
2024-06-26 02:11:46 +0400

    add contentUser

--- app/contentUser/content.js
+++ app/contentUser/content.js
@@ -6,9 +6,9 @@ function ContentUser() {
       <div className="content-container" >
 
           <div className="content-left">
-              <p>FEATURED</p>
+              <p className="p-item" >FEATURED</p>
               <h2 className="content-p-text" >How To Fly A Private Jet On Your Next Trip</h2>
-              <p>The state of Utah in the United States is home to lots of beautiful National Parks, & Bryce Canyon National Park ranks as three of the most magnificent & awe inspiring.</p>
+              <p >The state of Utah in the United States is home to lots of beautiful National Parks, & Bryce Canyon National Park ranks as three of the most magnificent & awe inspiring.</p>
               <p className="p-button" >READ ARTICLE</p>
           </div>
 
@@ -20,7 +20,7 @@ function ContentUser() {
                 <img src="./imgs/Avatar (1).svg"/>
                 <div className="content-bock-box-row" >
                     <h3>Do A Sporting Stag Do In Birmingham</h3>
-                    <p>13 Dec 2019</p>
+                    <p className="p-block-item" >13 Dec 2019</p>
                 </div>
               </div>
 
@@ -28,7 +28,7 @@ function ContentUser() {
                 <img src="./imgs/Avatar (2).svg"/>
                 <div className="content-bock-box-row" >
                     <h3>From Wetlands To Canals And Dams Amsterdam Is Alive</h3>
-                    <p>13 Dec 2019</p>
+                    <p className="p-block-item" >13 Dec 2019</p>
                 </div>
               </div>
 
@@ -36,7 +36,7 @@ function ContentUser() {
                 <img src="./imgs/Avatar.svg"/>
                 <div className="content-bock-box-row" >
                     <h3>Stu Unger Rise And Fall Of A Poker Genius</h3>
-                    <p>13 Dec 2019</p>
+                    <p className="p-block-item" >13 Dec 2019</p>
                 </div>
               </div>
 
@@ -44,7 +44,7 @@ function ContentUser() {
                 <img src="./imgs/Avatar (1).svg"/>
                 <div className="content-bock-box-row" >
                     <h3>Stu Unger Rise And Fall Of A Poker Genius</h3>
-                    <p>13 Dec 2019</p>
+                    <p className="p-block-item" >13 Dec 2019</p>
                 </div>
               </div>
 
