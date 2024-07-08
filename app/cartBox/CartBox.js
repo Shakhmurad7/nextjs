f4ed5bf797fabf37e38d1fa1b4f2a02409d1ad29
Shakhmurad <muradb356@gmail.com>
2024-07-09 03:15:32 +0400

    add text to img

--- app/cartBox/CartBox.js
+++ app/cartBox/CartBox.js
@@ -10,21 +10,56 @@ function CartBox() {
         </div>
 
         <div className='cartBox-container'>
+
             <div className='cartBox-block-one' >
-                <div className='block-one-div-1'></div>
-                <div className='block-one-div-2'></div>
+                <div className='block-one-div-1'>
+                <div className='text-position-cart'>
+                            <p>DESTINATIONS</p>
+                            <h2>A Guide To Rocky Mountain Vacations</h2>
+                        </div>
+                </div>
+                <div className='block-one-div-2'>
+                <div className='text-position-cart'>
+                            <p>DESTINATIONS</p>
+                            <h2>Traveling To Barcelona</h2>
+                        </div>
+                </div>
             </div>
+
             <div className='cartBox-block-two' >
                 <div className='block-two-row-div' >
-                    <div className='block-two-div-img-1'></div>
-                    <div className='block-two-div-img-2'></div>
+                    <div className='block-two-div-img-1'>
+                        <div className='text-position-cart'>
+                            <p>DESTINATIONS</p>
+                            <h2>Party Jokes Startling But Unnecessary</h2>
+                        </div>
+                    </div>
+                    <div className='block-two-div-img-2'>
+                    <div className='text-position-cart'>
+                            <p>DESTINATIONS</p>
+                            <h2>Maui By Air The Best Way Around The Island</h2>
+                        </div>
+                    </div>
                 </div>
-                < div className='block-two-div-2'></div>
+                < div className='block-two-div-2'>
+                </div>
             </div>
+
             <div className='cartBox-block-three' >
-                <div className='block-three-div-1'></div>
-                <div className='block-three-div-2'></div>
+                <div className='block-three-div-1'>
+                <div className='text-position-cart'>
+                            <p>DESTINATIONS</p>
+                            <h2>Traveling To Barcelona</h2>
+                        </div>
+                </div>
+                <div className='block-three-div-2'>
+                <div className='text-position-cart'>
+                            <p>DESTINATIONS</p>
+                            <h2>A Guide To Rocky Mountain Vacations</h2>
+                        </div>
+                </div>
             </div>
+
         </div>
     </div>
   )
